35696b701a14d382e490810befcedbd97e6f07a9
Araecy <74868163+Araecy@users.noreply.github.com>
2022-10-11 19:35:23 +0200

    Main

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/index.css">
+</head>
+<body>
+    <div id="navWrapper">
+        <nav>
+            <li> <a href="#">home</a> </li>
+            <li> <a href="#">collection</a> </li>
+            <!-- note if user is loged in change login/register -->
+            <li> <a href="#">login/register</a> </li>
+
+            <div id="logoWrapper">
+                <img src="logo.png" alt="logo" id="logo">
+            </div>
+
+        </nav>
+    </div>
+</body>
+</html>
