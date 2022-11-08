fc62f9437e7ef5aee3ed5774a4accb8a7865a52a
bas923 <boktor638@gmail.com>
2022-11-08 11:48:11 +0100

    update

--- index.php
+++ index.php
@@ -17,10 +17,31 @@
         <link rel="stylesheet" href="css/signupAndLoginStyle.css">
     </head>
     <body>
+        <!-- Background -->
         <div id="backgroundWrapper"></div>
+
+        <!-- Navbar and modals -->
         <?php
             require_once("navbar.php");
             require_once("modals.php");
         ?>
+
+        <!-- Text -->
+        <div id="textDiv">
+            <h1>body pillow collectors</h1>
+            <p>
+                Welcome to body&nbsp;pillow&nbsp;collectors, this website is made to
+                store body pillows in the cloud so you can always access them. On the website
+                you can see pillow covers that people have uploaded themselves and you can too!
+                this is very easy, because special pages have been created where you only have
+                to fill out an input list. this entry list consists of a few things: an image,
+                character&nbsp;name, series, size, material, and&nbsp;the&nbsp;author. After you've
+                done that you can go to the overview page to see your list and you can also see
+                other people's pillows. And finally, you can also change things in the lists if
+                something is wrong in it. then you can click on edit and you get the same list
+                from the input page only then there is already data in it, and when you are
+                done editing things, you can press submit and the list is updated.
+            </p>
+        </div>
     </body>
 </html>
